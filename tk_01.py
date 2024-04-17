d15ba1c358c072bbd422cd4c545d0120470faa1d
joaostr22 <143971003+joaostr22@users.noreply.github.com>
2024-04-17 10:45:24 -0300

    Update tk_01.py

--- tk_01.py
+++ tk_01.py
@@ -18,11 +18,22 @@ CREATE TABLE IF NOT EXISTS alunos (
     curso TEXT,
     endereco TEXT,
     telefone TEXT,
-    email TEXT,
-    nome_responsavel TEXT,
-    telefone_responsavel TEXT
+    email TEXT
 )
 """)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS prof (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    idade INTEGER,
+    disciplina TEXT,
+    endereco TEXT,
+    telefone TEXT,
+    email TEXT
+)
+""")
+
 conn.commit()
 
 
@@ -54,6 +65,26 @@ def registrar_aluno():
 
     limpar_campos()
     messagebox.showinfo("Sucesso", "Aluno registrado com sucesso!")
+    
+def registrar_prof():
+    nome = entrada_nome_prof.get()
+    idade = int(entrada_idade_prof.get())
+    disciplina = entrada_disciplina_prof.get()
+    endereco = entrada_endereco_prof.get()
+    telefone = entrada_telefone_prof.get()
+    email = entrada_email_prof.get()
+    
+
+    sql = """
+    INSERT INTO prof (nome, idade, disciplina, endereco, telefone, email)
+    VALUES (?, ?, ?, ?, ?, ?)
+    """
+    valores = (nome, idade, disciplina, endereco, telefone, email)
+    cursor.execute(sql, valores)
+    conn.commit()
+
+    limpar_campos()
+    messagebox.showinfo("Sucesso", "Professor registrado com sucesso!")
 
 def limpar_campos():
     entrada_nome_aluno.delete(0, tk.END)
@@ -62,6 +93,12 @@ def limpar_campos():
     entrada_endereco_aluno.delete(0, tk.END)
     entrada_telefone_aluno.delete(0, tk.END)
     entrada_email_aluno.delete(0, tk.END)
+    entrada_nome_prof.delete(0, tk.END)
+    entrada_idade_prof.delete(0, tk.END)
+    entrada_disciplina_prof.delete(0, tk.END)
+    entrada_endereco_prof.delete(0, tk.END)
+    entrada_telefone_prof.delete(0, tk.END)
+    entrada_email_prof.delete(0, tk.END)
     
 
 # Estilo para os frames
@@ -82,17 +119,21 @@ abas.add(aba_prof, text='Cadastrar Professor')
 
 
 # Cabeçalho universidade
-cabecalho = tk.Label(aba_aluno, text="Universidade X", font=("Cambria", 35), image=logo, compound='left')
-cabecalho.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
+cabecalho01 = tk.Label(aba_aluno, text="Universidade X", font=("Cambria", 35), image=logo, compound='left')
+cabecalho01.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
+cabecalho02 = tk.Label(aba_prof, text="Universidade X", font=("Cambria", 35), image=logo, compound='left')
+cabecalho02.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
 
 
 # Frames
 fr_cadastro01 = Frame(aba_aluno, borderwidth=1, relief="solid", bg="#E7DFDF")
 fr_cadastro01.place(x=160, y=210, width=500, height=300)
+fr_cadastro02 = Frame(aba_prof, borderwidth=1, relief="solid", bg="#E7DFDF")
+fr_cadastro02.place(x=160, y=210, width=500, height=300)
 
 
 # Formulário alunos
-label_explicativo = tk.Label(aba_aluno, text="Cadastro de alunos:", font=("Cambria", 20, "bold"), fg="black", bg="#F0F0F0")
+label_explicativo = tk.Label(aba_aluno, text="Cadastro de Alunos:", font=("Cambria", 20, "bold"), fg="black", bg="#F0F0F0")
 label_explicativo.place(relx=0.5, rely=0.24, anchor=tk.CENTER)
 
 #
@@ -143,6 +184,63 @@ botao_enviar.place(relx=0.6, rely=0.85, anchor=tk.CENTER)
 
 botao_limpar = tk.Button(aba_aluno, text="Limpar", command=limpar_campos, font=("Arial", 14), fg="white", bg="#a94040")
 botao_limpar.place(relx=0.4, rely=0.85, anchor=tk.CENTER)
+
+
+#--------------------------------------------------------------------------------------
+
+
+# Formulário professores
+label_explicativo02 = tk.Label(aba_prof, text="Cadastro de Professores:", font=("Cambria", 20, "bold"), fg="black", bg="#F0F0F0")
+label_explicativo02.place(relx=0.5, rely=0.24, anchor=tk.CENTER)
+
+#
+label_nome_prof = tk.Label(fr_cadastro02, text="Nome:", font=("Arial", 12), fg="black", bg="#E7DFDF")
+label_nome_prof.place(relx=0.35, rely=0.1, anchor=tk.E) #antes 0.3
+
+entrada_nome_prof = tk.Entry(fr_cadastro02, font=("Arial", 12))
+entrada_nome_prof.place(relx=0.7, rely=0.1, anchor=tk.CENTER)
+
+#
+label_idade_prof = tk.Label(fr_cadastro02, text="Idade:", font=("Arial", 12), fg="black", bg="#E7DFDF")
+label_idade_prof.place(relx=0.35, rely=0.25, anchor=tk.E)
+
+entrada_idade_prof = tk.Entry(fr_cadastro02, font=("Arial", 12))
+entrada_idade_prof.place(relx=0.7, rely=0.25, anchor=tk.CENTER)
+
+#
+label_disciplina_prof = tk.Label(fr_cadastro02, text="Disciplina:", font=("Arial", 12), fg="black", bg="#E7DFDF")
+label_disciplina_prof.place(relx=0.35, rely=0.4, anchor=tk.E)
+
+entrada_disciplina_prof = tk.Entry(fr_cadastro02, font=("Arial", 12))
+entrada_disciplina_prof.place(relx=0.7, rely=0.4, anchor=tk.CENTER)
+
+#
+label_endereco_prof = tk.Label(fr_cadastro02, text="Endereço:", font=("Arial", 12), fg="black", bg="#E7DFDF")
+label_endereco_prof.place(relx=0.35, rely=0.55, anchor=tk.E)
+
+entrada_endereco_prof = tk.Entry(fr_cadastro02, font=("Arial", 12))
+entrada_endereco_prof.place(relx=0.7, rely=0.55, anchor=tk.CENTER)
+
+#
+label_telefone_prof = tk.Label(fr_cadastro02, text="Telefone:", font=("Arial", 12), fg="black", bg="#E7DFDF")
+label_telefone_prof.place(relx=0.35, rely=0.70, anchor=tk.E)
+
+entrada_telefone_prof = tk.Entry(fr_cadastro02, font=("Arial", 12))
+entrada_telefone_prof.place(relx=0.7, rely=0.70, anchor=tk.CENTER)
+
+#
+label_email_prof = tk.Label(fr_cadastro02, text="Email:", font=("Arial", 12), fg="black", bg="#E7DFDF")
+label_email_prof.place(relx=0.35, rely=0.85, anchor=tk.E)
+
+entrada_email_prof = tk.Entry(fr_cadastro02, font=("Arial", 12))
+entrada_email_prof.place(relx=0.7, rely=0.85, anchor=tk.CENTER)
+
+#
+botao_enviar02 = tk.Button(aba_prof, text="Enviar", command=registrar_aluno, font=("Arial", 14), fg="white", bg="#a94040")
+botao_enviar02.place(relx=0.6, rely=0.85, anchor=tk.CENTER)
+
+botao_limpar02 = tk.Button(aba_prof, text="Limpar", command=limpar_campos, font=("Arial", 14), fg="white", bg="#a94040")
+botao_limpar02.place(relx=0.4, rely=0.85, anchor=tk.CENTER)
 
 
 
